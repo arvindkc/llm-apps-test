75922a94e31261078e30d1c2e72c2726853c56fd
innov8tr4eva <arvindkc@gmail.com>
2024-02-25 11:24:41 -0800

    Working code for linkedin summary given a url

---
+++ app.py
@@ -0,0 +1,43 @@
+import json
+import streamlit as st
+from langchain.chains import LLMChain
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from third_parties.linkedin import scrape_linkedin_profile
+
+
+from dotenv import load_dotenv
+import pprint
+load_dotenv()
+
+
+
+if __name__ == '__main__':
+    linked_in_profile_url = st.text_input("Enter the linkedin profile url", "https://www.linkedin.com/in/arvindkc/")
+    # linked_in_profile_url = "https://www.linkedin.com/in/arvindkc/"
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linked_in_profile_url)
+    #linkedin_data_str = json.dumps(linkedin_data, indent=4)
+    # print(linkedin_data_str)
+    # print(os.getenv('OPENAI_API_KEY'))
+    
+    summary_template = """ 
+    given the linked information {information} about a person I want you to create:
+    1. A short summary
+    2. A summary of their key technical skills
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm =ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    response = chain.invoke(input={"information":linkedin_data})
+    response_str = json.dumps(response["text"], indent=4)
+    response_dict = json.loads(response_str)
+    print(response_dict)
+
+    st.header("Linkedin Profile Summary", divider='blue')
+    st.write('Showing linkedin profile summary for the profile: ', linked_in_profile_url)
+    st.write(response_dict)
---
+++ third_parties/linkedin.py
@@ -0,0 +1,37 @@
+import os
+import requests 
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str):
+    '''
+    This function scrapes a linkedin profile and returns the following information:
+    '''
+    
+    
+
+    
+    # Method 1
+    # response = requests.get("https://gist.githubusercontent.com/arvindkc/8fac136752fbcdc6ca3da9df6307eb8d/raw/085905a6b4af0a6b6e3fcf2fca1fe8ec2da6a60c/gistfile1.txt")
+
+    # Method 2 - works
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+
+    response = requests.get(
+        api_endpoint, params={"url": linkedin_profile_url}, headers=header_dic
+    )
+
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+    
